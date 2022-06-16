2d74f8af68ef80a1e67e00077cfcf025712e0e72
pasha <pashaganiev20@gmail.com>
2022-06-16 15:01:48 +0300

    fix:error

--- main.js
+++ main.js
@@ -25,6 +25,33 @@ function updateTiming(){
     
 }
 setInterval(updateTiming, 1000);
+let currentDay1 = new Date().getFullYear();
+let nextDay1 = new Date(`May 10 ${currentDay + 1} 00:00:00`);
+
+
+function updateTiming(){
+    let currentTime =  new Date();
+    let difference = nextDay - currentTime;
+    
+    let daysLeft = Math.floor(difference / 1000 / 60 / 60 / 24);//days
+    let hoursLeft = Math.floor(difference / 1000 / 60 / 60) % 24;//hours
+    let minutesLeft = Math.floor(difference / 1000 / 60 ) % 60;//minutes
+    let secondsLeft = Math.floor(difference / 1000) % 60;//seconds
+    
+    
+    let days = document.querySelector('#days1');
+    let hours = document.querySelector('#hours1');
+    let minutes = document.querySelector('#minutes1');
+    let seconds = document.querySelector('#seconds1');
+    
+    // days.innerText = daysLeft;
+    
+    hours.innerText = hoursLeft < 10 ? '0' + hoursLeft : hoursLeft;
+    minutes.innerText = minutesLeft < 10 ? '0' + minutesLeft : minutesLeft;
+    seconds.innerText = secondsLeft < 10 ? '0' + secondsLeft : secondsLeft;
+    
+}
+setInterval(updateTiming, 1000);
 
 let menu = document.querySelector('.burger')
 let close = document.querySelector('.burger__active')
